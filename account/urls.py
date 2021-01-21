db331cb41e25c6f1fcb45399978bc57541edfff6
echegeni <best.rank@ymail.com>
2021-01-21 12:08:44 +0330

    add dashboard url

--- account/urls.py
+++ account/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('sign_up/', views.sign_up, name='sign_up'),
     path('sign_out/', views.sign_out, name='sign_out'),
     path('profile/', views.profile, name='profile'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('profile/change_password/', views.change_password, name='change_password'),
 ]
